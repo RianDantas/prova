3335b842625d0a1242596943a9f0e6d10752afc6
RianDantas <134387076+RianDantas@users.noreply.github.com>
2024-07-26 13:06:20 +0000

    f#	modified:   departamento/loja/views.py

--- departamento/loja/views.py
+++ departamento/loja/views.py
@@ -3,33 +3,25 @@ from .models import Produto
 from django.http import HttpResponse
 # Create your views here.
 
-
 # Retornando elementos apenas da categoria ferramentas
 def ferramentas(request):
     produto = Produto.objects.filter(categoria__contains="Ferramentas")
     return HttpResponse(produto)
-
 
 # Retornando com 0 estoque
 def outEstoque(request):
     produto = Produto.objects.filter(quantidade="0")
     return HttpResponse(produto)
 
-
-
 # Retornando com 0 estoque e categoria informatica
 def outEstoqueInformatica(request):
     produto = Produto.objects.filter(categoria="Informatica", quantidade="0")
     return HttpResponse(produto)
 
-
-
 # Retornando com peso entre 100g e 1000g
 def peso(request):
     produto = Produto.objects.filter(peso__gte="100", peso__lte="1000")
     return HttpResponse(produto)
-
-
 
 # Retornando todos os elementos com máximo de 5
 def tudo(request):
